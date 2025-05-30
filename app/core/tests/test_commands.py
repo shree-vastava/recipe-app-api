395cc83a5b875dff0ef7cdfca2f7f5c1f6939f06
prakhar <srivastava.prakhar91@gmail.com>
2025-05-30 18:10:04 +0530

    removed whitespaces from import

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -3,9 +3,7 @@ Test custom django management commands
 """
 
 from unittest.mock import patch
-
 from psycopg2 import OperationalError as Psycopg2OpError
-
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
